ff1ca233fc86237654d19f186c3d7ac3b18afa58
ForestoAY <forestoay@gmail.com>
2024-10-01 14:37:17 +0700

    feat: check if username or slug exists or not

--- memerize/src/app/api/posts/[username]/[slug]/route.ts
+++ memerize/src/app/api/posts/[username]/[slug]/route.ts
@@ -6,7 +6,9 @@ export async function GET(request: Request, { params }: { params: { username: st
   const { username, slug } = params;
   try {
     const onePost = await PostModel.findOneByUsernameAndSlug(username, slug);
-
+    if (!onePost) {
+      throw new Error("Post not found")
+    }
     return NextResponse.json(onePost);
   } catch (error) {
     return handleError(error);
