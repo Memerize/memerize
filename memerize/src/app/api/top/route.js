c1191ae12f3a4acfba1162e095bdbfc43dccf8d6
ForestoAY <forestoay@gmail.com>
2024-10-03 15:22:40 +0700

    feat: add api/top (list top posts)

---
+++ memerize/src/app/api/top/route.js
@@ -0,0 +1,15 @@
+import { PostModel } from "@/models/PostModel";
+import { handleError } from "@/helpers/handleError";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  try {
+    const posts = await PostModel.findTopPost();
+
+    return new NextResponse(JSON.stringify(posts), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return handleError(error);
+  }
+}
