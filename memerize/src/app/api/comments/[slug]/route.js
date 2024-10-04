b8e72f02a2fdbcb0523e777c3c0ae0b33ec23289
ForestoAY <forestoay@gmail.com>
2024-10-04 11:16:45 +0700

    feat: user now can comment on the post

---
+++ memerize/src/app/api/comments/[slug]/route.js
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import { PostModel } from "@/models/PostModel";
+import { UserModel } from "@/models/UserModel";
+import { handleError } from "@/helpers/handleError";
+
+export async function POST(request, { params }) {
+  const { slug } = params;
+  const { content } = await request.json();
+  const username = request.headers.get("x-user-username");
+  console.log(username);
+  
+  try {
+    const post = await PostModel.findOneBySlug(slug);
+    if (!post) {
+      throw new Error("Post not found");
+    }
+
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const newComment = {
+      username: user.username,
+      content,
+      replies: [],
+      commentLikes: [],
+    };
+
+    const updatedPost = await PostModel.addComment(slug, newComment);
+
+    const addedComment = updatedPost.comments[updatedPost.comments.length - 1];
+    return NextResponse.json(addedComment);
+  } catch (error) {
+    return handleError(error);
+  }
+}
