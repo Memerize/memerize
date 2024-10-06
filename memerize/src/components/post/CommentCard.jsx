fa02433b20ea60b1af933e0535d300f54babf5b7
ForestoAY <forestoay@gmail.com>
2024-10-06 15:28:31 +0700

    refactor: add link to profile user when click on profile image comment and replies

--- memerize/src/components/post/CommentCard.jsx
+++ memerize/src/components/post/CommentCard.jsx
@@ -117,11 +117,13 @@ export default function CommentCard({ comment, slug, onReplyAdded }) {
   return (
     <div className="bg-gray-100 p-4 rounded-md shadow">
       <div className="flex items-center space-x-4 mb-2">
-        <img
-          src={comment.userImage?.image || "https://via.placeholder.com/50"}
-          alt={comment.username}
-          className="w-8 h-8 rounded-full object-cover"
-        />
+        <Link href={`/posts/${comment.username}`}>
+          <img
+            src={comment.userImage?.image || "https://via.placeholder.com/50"}
+            alt={comment.username}
+            className="w-8 h-8 rounded-full object-cover"
+          />
+        </Link>
         <div>
           <p className="font-semibold text-gray-800">{comment.username}</p>
           <span className="text-sm text-gray-500">
--- memerize/src/components/post/ReplyCard.jsx
+++ memerize/src/components/post/ReplyCard.jsx
@@ -35,11 +35,13 @@ export default function ReplyCard({ reply }) {
   return (
     <div className="bg-gray-200 p-2 rounded-md shadow-sm text-sm text-black">
       <div className="flex items-center space-x-4 mb-1">
-        <img
-          src={reply.userImage?.image || "https://via.placeholder.com/50"}
-          alt={reply.username}
-          className="w-6 h-6 rounded-full object-cover"
-        />
+        <Link href={`/posts/${reply.username}`}>
+          <img
+            src={reply.userImage?.image || "https://via.placeholder.com/50"}
+            alt={reply.username}
+            className="w-6 h-6 rounded-full object-cover"
+          />
+        </Link>
         <p className="font-semibold">{reply.username}</p>
       </div>
       <p className="text-gray-700">{parseMentions(reply.content)}</p>
