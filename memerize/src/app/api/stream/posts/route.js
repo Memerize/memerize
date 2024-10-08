38dcd180536b981ed94b86ebcee3bb1ca2e4566c
ForestoAY <forestoay@gmail.com>
2024-10-08 15:26:38 +0700

    feat: add api/stream/posts

---
+++ memerize/src/app/api/stream/posts/route.js
@@ -0,0 +1,26 @@
+import { PostModel } from "@/models/PostModel";
+
+export async function GET() {
+  return new Response(
+    (async function* () {
+      let lastPostCount = await PostModel.collection().countDocuments();
+
+      while (true) {
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        const newPostCount = await PostModel.collection().countDocuments();
+
+        if (newPostCount > lastPostCount) {
+          yield `data: ${newPostCount - lastPostCount} new posts available\n\n`;
+          lastPostCount = newPostCount;
+        }
+      }
+    })(),
+    {
+      headers: {
+        "Content-Type": "text/event-stream",
+        Connection: "keep-alive",
+        "Cache-Control": "no-cache",
+      },
+    }
+  );
+}
